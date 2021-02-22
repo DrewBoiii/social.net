02c08a1ef6958f15bff2cc9424515c6a6c14f03a
DrewBoiii <goodchickenboy@mail.ru>
2021-02-22 12:03:09 +0400

    add rendering data from server

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,4 +1,21 @@
 
+let messageApi = Vue.resource('/message{/id}');
+
+Vue.component('message-row', {
+    props: ['message'],
+    template: '<div><li>{{ message.id }} - {{ message.content }}</li></div>'
+});
+
+Vue.component('messages-list', {
+    props: ['messages'],
+    template: '<div><message-row v-for="message in messages" :key="message.id" :message="message" /></div>',
+    created: function () {
+        messageApi.get()
+            .then(response => response.json()
+                .then(data => data.forEach(message => this.messages.push(message))))
+    }
+});
+
 let app = new Vue({
     el: '#app',
     template: '<messages-list :messages="messages" />',
