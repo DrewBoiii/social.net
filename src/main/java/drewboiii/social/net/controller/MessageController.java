b183577a6dfa96069302dd7cdafb26d663de8697
DrewBoiii <goodchickenboy@mail.ru>
2021-06-17 22:40:41 +0400

    add database, useful libs

--- src/main/java/drewboiii/social/net/controller/MessageController.java
+++ src/main/java/drewboiii/social/net/controller/MessageController.java
@@ -1,69 +1,51 @@
 package drewboiii.social.net.controller;
 
+import com.fasterxml.jackson.annotation.JsonView;
 import drewboiii.social.net.exception.NotFoundException;
+import drewboiii.social.net.persistence.model.Message;
+import drewboiii.social.net.persistence.repo.MessageRepository;
+import drewboiii.social.net.util.MessageViews;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.BeanUtils;
 import org.springframework.web.bind.annotation.*;
 
-import java.security.SecureRandom;
-import java.util.*;
+import java.util.List;
+import java.util.UUID;
 
+@AllArgsConstructor
 @RestController
 @RequestMapping("message")
 public class MessageController {
 
-    private static final List<Map<String, String>> messages = new ArrayList<Map<String, String>>() {{
-        add(new HashMap<String, String>() {{
-            put("uid", UUID.randomUUID().toString());
-            put("id", "1");
-            put("content", "message content");
-        }});
-        add(new HashMap<String, String>() {{
-            put("uid", UUID.randomUUID().toString());
-            put("id", "2");
-            put("content", "message content");
-        }});
-        add(new HashMap<String, String>() {{
-            put("uid", UUID.randomUUID().toString());
-            put("id", "3");
-            put("content", "message content");
-        }});
-    }};
+    private final MessageRepository messageRepository;
 
     @GetMapping
-    public List<Map<String, String>> getMessages() {
-        return messages;
+    @JsonView(MessageViews.Public.class)
+    public List<Message> getMessages() {
+        return messageRepository.findAll();
     }
 
     @GetMapping("{id}")
-    public Map<String, String> getMessage(@PathVariable Long id) {
-        return getMessageById(id);
+    @JsonView(MessageViews.FullMessage.class)
+    public Message getMessage(@PathVariable Long id) {
+        return messageRepository.findById(id).orElseThrow(NotFoundException::new);
     }
 
     @PostMapping
-    public Map<String, String> saveMessage(@RequestBody Map<String, String> message) {
-        message.put("uid", UUID.randomUUID().toString());
-        message.put("id", String.valueOf(new SecureRandom().longs().filter(value -> value > 0).findFirst().orElse(-1)));
-        messages.add(message);
-        return message;
+    public Message saveMessage(@RequestBody Message message) {
+        message.setUid(UUID.randomUUID().toString());
+        return messageRepository.save(message);
     }
 
     @PutMapping("{id}")
-    public Map<String, String> putMessage(@PathVariable Long id, @RequestBody Map<String, String> editedMessage) {
-        Map<String, String> message = getMessageById(id);
-        message.putAll(editedMessage);
-        return message;
+    public Message putMessage(@PathVariable(value = "id") Message oldMessage, @RequestBody Message newMessage) {
+        BeanUtils.copyProperties(newMessage, oldMessage, "id");
+        return messageRepository.save(oldMessage);
     }
 
     @DeleteMapping("{id}")
     public void deleteMessage(@PathVariable Long id) {
-        Map<String, String> messageToDelete = getMessage(id);
-        messages.remove(messageToDelete);
-    }
-
-    private Map<String, String> getMessageById(Long id) {
-        return messages.stream()
-                .filter(message -> message.get("id").contentEquals(id.toString()))
-                .findFirst()
-                .orElseThrow(NotFoundException::new);
+        messageRepository.deleteById(id);
     }
 
 }
---
+++ src/main/java/drewboiii/social/net/persistence/model/Message.java
@@ -0,0 +1,43 @@
+package drewboiii.social.net.persistence.model;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonView;
+import drewboiii.social.net.util.MessageViews;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table
+public class Message {
+
+    @JsonView(MessageViews.FullMessage.class)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @JsonView(MessageViews.FullMessage.class)
+    private String uid;
+
+    @JsonView(MessageViews.Public.class)
+    private String content;
+
+    @JsonView(MessageViews.Public.class)
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm")
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    void prePersist() {
+        this.createdAt = LocalDateTime.now();
+    }
+
+}
